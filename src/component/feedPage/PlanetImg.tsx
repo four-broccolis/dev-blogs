76a31c4a370fa30b41eae0c2f0d209cb6fbbc9b4
nbc7799 <nbc7799@naver.com>
2022-02-09 21:07:48 +0900

    행성이미지추가,리사이즈

--- src/component/feedPage/PlanetImg.tsx
+++ src/component/feedPage/PlanetImg.tsx
@@ -1,14 +1,13 @@
 import styled from "styled-components";
 
 export default function PlanetImg<T>(props: T) {
-  let imageArray = [1,2,3,4,5,6,7,8,9,10];
+  let imageArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
   let newNum = []
-  while(imageArray.length > 9){
+  while(imageArray.length > 17){
     let moveNum =imageArray.splice(Math.floor(Math.random() * imageArray.length))[0]
-    console.log(moveNum)
     newNum.push(moveNum)
+    console.log(newNum)
   }
-  console.log(newNum)
   return <Image src={process.env.PUBLIC_URL + `/images/planet${newNum}.png`}></Image>;
 }
 
